9a111174b55b762ddac07d0ae0140538473c758a
alanhou <info@iscbj.com>
2021-01-02 00:54:56 +0800

    第三章 创建Odoo插件模块

---
+++ Chapter03/03_file_structure/my_library/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': ['base'],
+    # 需注释以下文件或为 xml 添加基本标签结构
+    #'data': ['views/views.xml'],
+    #'demo': ['demo.xml'],
+}
---
+++ Chapter03/05_menu_views/my_library/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': ['views/views.xml'],
+    #'demo': ['demo.xml'],
+}
