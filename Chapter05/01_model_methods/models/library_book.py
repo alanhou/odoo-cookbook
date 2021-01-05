c8be122f0cd941bafa399e34d5374b04e1126aa8
alanhou <info@iscbj.com>
2021-01-05 18:15:30 +0800

    定义模型方法及使用API装饰器

---
+++ Chapter05/01_model_methods/models/library_book.py
@@ -0,0 +1,44 @@
+from odoo import models, fields, api
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book'
+    name = fields.Char('Title', required=True)
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many(
+        'res.partner',
+        string='Authors'
+    )
+    state = fields.Selection([
+        ('draft', 'Unavailable'),
+        ('available', 'Available'),
+        ('borrowed', 'Borrowed'),
+        ('lost', 'Lost')],
+        'State', default="draft")
+
+    @api.model
+    def is_allow_transition(self, old_state, new_state):
+        allowed = [('draft', 'available'),
+                   ('available', 'borrowed'),
+                   ('borrowed', 'available'),
+                   ('available', 'lost'),
+                   ('borrowed', 'lost'),
+                   ('lost', 'available')]
+        return (old_state, new_state) in allowed
+
+    def change_state(self, new_state):
+        for book in self:
+            if book.is_allow_transition(book.state, new_state):
+                book.state = new_state
+            else:
+                continue
+
+    def make_available(self):
+        self.change_state('available')
+
+    def make_borrowed(self):
+        self.change_state('borrowed')
+
+    def make_lost(self):
+        self.change_state('lost')
