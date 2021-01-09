d2b0c056c163622d556180f976643175f0b41a1c
alanhou <info@iscbj.com>
2021-01-10 00:42:02 +0800

    继承write()和create()

---
+++ Chapter05/12_extend_create_write/models/library_book.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book'
+    name = fields.Char('Title', required=True)
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many(
+        'res.partner',
+        string='Authors'
+    )
+    manager_remarks = fields.Text('Manager Remarks')
+
+    @api.model
+    def create(self, values):
+        if not self.user_has_groups('my_library.group_librarian'):
+            if 'manager_remarks' in values:
+                raise UserError(
+                    'You are not allowed to add manager_remarks'
+                )
+        return super(LibraryBook, self).create(values)
+
+    def write(self, values):
+        if not self.user_has_groups('my_library.group_librarian'):
+            if 'manager_remarks' in values:
+                raise UserError(
+                    'You are not allowed to modify manager_remarks'
+                )
+        return super(LibraryBook, self).write(values)
