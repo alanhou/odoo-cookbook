8d42d869df565e7ff51c7dafd1ac017b4e87222d
alanhou <info@iscbj.com>
2021-03-02 11:17:06 +0800

    使用变更的上下文调用方法

---
+++ Chapter08/02_call_with_context/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/library_book_categ.xml',
+        'views/library_book_rent.xml',
+    ],
+    # 'demo': ['demo.xml'],
+}
