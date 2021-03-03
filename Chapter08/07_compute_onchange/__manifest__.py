bc709a63c568cd3b57d27fa5ca8fde1463e02e9b
alanhou <info@iscbj.com>
2021-03-03 14:26:20 +0800

    借助compute方法调用onchange

---
+++ Chapter08/07_compute_onchange/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/library_book_categ.xml',
+        'views/library_book_rent.xml',
+        'wizard/library_book_rent_wizard.xml',
+        'wizard/library_book_return_wizard.xml',
+    ],
+    # 'demo': ['demo.xml'],
+}
