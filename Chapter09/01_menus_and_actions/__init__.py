f30de524cf8c2dc1b29574514656152df40f8cf0
alanhou <info@iscbj.com>
2021-03-04 10:28:04 +0800

    添加菜单项和窗口动作

---
+++ Chapter09/01_menus_and_actions/__init__.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, SUPERUSER_ID
+from . import models
+from . import controllers
+from . import wizard
+
+
+def add_book_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    book_data1 = {'name': 'Book 1', 'date_release': fields.Date.today()}
+    book_data2 = {'name': 'Book 2', 'date_release': fields.Date.today()}
+    env['library.book'].create([book_data1, book_data2])
