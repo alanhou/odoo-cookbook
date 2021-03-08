d6c415f368b1d4c3d1ee44cb1244f03a51a1206c
alanhou <info@iscbj.com>
2021-03-08 09:21:35 +0800

    创建OWL组件

---
+++ Chapter16/01_component/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        "views/templates.xml",
+    ],
+    # 'demo': ['demo.xml'],
+}
