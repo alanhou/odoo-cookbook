e429aca154fc5769c4de44f8c3594e0a3a4e611d
alanhou <info@iscbj.com>
2021-04-26 21:19:17 +0800

    创建安全组并分配用户

---
+++ Chapter10/01_security_groups/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Library',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        "views/library_book.xml",
+    ],
+    # 'demo': ['demo.xml'],
+}
