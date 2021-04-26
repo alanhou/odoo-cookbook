600cd70fabb2f366297d88892656a439847048c4
alanhou <info@iscbj.com>
2021-04-27 07:20:29 +0800

    使用记录规则限制记录访问

---
+++ Chapter10/04_record_rules/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Library',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'security/security_rules.xml',
+        "views/library_book.xml",
+    ],
+    # 'demo': ['demo.xml'],
+}
