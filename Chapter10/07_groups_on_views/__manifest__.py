d524fa7f100a63d13a27a2d9236c91815e084702
alanhou <info@iscbj.com>
2021-04-27 10:45:54 +0800

    根据组来隐藏视图元素和菜单

---
+++ Chapter10/07_groups_on_views/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Library',
+    'version': '14.0.1',
+    'depends': ['base_setup'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'security/security_rules.xml',
+        'views/res_config_settings.xml',
+        "views/library_book.xml",
+        'views/library_book_categ.xml',
+    ],
+    # 'demo': ['demo.xml'],
+}
