bb1f65a173de0aee88426e134d27fd0841f48daa
alanhou <info@iscbj.com>
2021-04-30 17:20:05 +0800

    定义文档样式表单

---
+++ Chapter09/11_document_styled_form/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Module",  # Module title
+    'description': """Long description""",  # 也可以使用rst格式
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': ['base', 'account'],
+    'data': [
+        'views/my_contacts.xml',
+    ],
+}
