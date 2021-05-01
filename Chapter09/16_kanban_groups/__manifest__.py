1bf883bf749ae0059b12e348c0a2262c99674e5b
alanhou <info@iscbj.com>
2021-05-01 09:40:04 +0800

    根据状态在列中显示看板卡片

---
+++ Chapter09/16_kanban_groups/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Project",  # Module title
+    'description': """Long description""",  # 也可以使用rst格式
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Project',
+    'version': '14.0.1',
+    'depends': ['project'],
+    'data': [
+        'views/my_tasks.xml',
+    ],
+}
