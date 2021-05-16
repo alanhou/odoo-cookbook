44797c335b1a18d98be534895411a433e4adb37d
alanhou <info@iscbj.com>
2021-05-16 19:25:27 +0800

    对记录启用存储选项

---
+++ Chapter12/13_archive_records/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Library',
+    'version': '14.0.1',
+    'depends': ['base_setup', 'contacts', 'mail'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        "views/library_book.xml",
+        'views/library_book_categ.xml',
+        'views/library_book_rent.xml',
+        'data/library_stage.xml',
+        'reports/book_rent_report.xml',
+        'reports/book_rent_templates.xml',
+    ],
+    # 'demo': ['demo.xml'],
+}
