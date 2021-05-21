d2f509cbe6bc3ec59db823c1d8f3656cce62ffac
alanhou <info@iscbj.com>
2021-05-21 10:32:23 +0800

    修改已有handler

---
+++ Chapter13/04_path_override/website_info/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website info",
+    'summary': "重写网站信息",
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': ['website'],
+    'data': [
+        'views/templates.xml'
+    ],
+}
