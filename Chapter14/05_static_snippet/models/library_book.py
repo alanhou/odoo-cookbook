9e47abd38a6940ae4b13b81a14bea593c2c167ac
alanhou <info@iscbj.com>
2021-05-22 23:10:18 +0800

    为用户提供静态小组件

---
+++ Chapter14/05_static_snippet/models/library_book.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book'
+
+    _order = 'date_release desc, name'
+
+    name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title', required=True)
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many('res.partner', string='Authors')
+    image = fields.Binary(attachment=True)
+    html_description = fields.Html()
+
+    def name_get(self):
+        """ This method used to customize display name of the record """
+        result = []
+        for record in self:
+            rec_name = "%s (%s)" % (record.name, record.date_release)
+            result.append((record.id, rec_name))
+        return result
