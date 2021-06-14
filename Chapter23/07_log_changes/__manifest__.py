a2e702babf7690dbb6adca661de89f49ef902002
alanhou <info@iscbj.com>
2021-06-14 08:43:33 +0800

    在聊天器中记录用户修改

---
+++ Chapter23/07_log_changes/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': "My library",
+    'summary': "轻松管理图书",
+    'description': """
+Manage Library
+==============
+Description related to library.
+     """,
+    'author': "Alan Hou",
+    'website': "https://alanhou.org",
+    'category': 'Tools',
+    'version': '14.0.1',
+    'depends': ['base', 'mail'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/library_book_categ.xml',
+        'views/library_book_rent.xml',
+        'data/mail_template.xml'
+    ],
+    "qweb": [
+        'static/src/xml/qweb_template.xml',
+    ],
+    # 'demo': ['demo.xml'],
+    # secret:32cc55e00fae4f0cb39ef316d76c9cc1
+}
